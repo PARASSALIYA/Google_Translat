d456a3e1eda404fd88f61b935a427450517d69b9
Paras Saliya <143994351+PARASSALIYA@users.noreply.github.com>
2024-12-30 11:39:40 +0530

    splash

--- lib/views/home/views/home_page.dart
+++ lib/views/home/views/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_translat/views/home/provider/home_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:google_translat/views/home/provider/home_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
---
+++ lib/views/splash/views/splash_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3)).then(
+      (value) => Navigator.pushReplacementNamed(context, 'home'),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 200,
+        ),
+      ),
+      backgroundColor: Colors.black,
+    );
+  }
+}
